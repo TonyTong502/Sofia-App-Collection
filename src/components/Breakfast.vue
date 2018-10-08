8ba53c2e621e3b418fdaac3bdf91c5f9e518f750
TonyTong502 <2098261@qq.com>
2018-10-08 23:01:36 +0800

    1 day make ONLY 1 selection

--- src/components/Breakfast.vue
+++ src/components/Breakfast.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="breakfast">
     <h1>{{ title }}</h1>
+    <h2>一天只能选择一次</h2>
     <div> 今天是 {{ date }}</div>
     <div>所有选项</div>
     <ol>
@@ -9,56 +10,65 @@
       </li>
     </ol>
     <div>
-      <button v-on:click="helpMe">帮我选吧！</button>
+      <button v-on:click="helpMe" :disabled=selected>{{helpText}}</button>
     </div>
     <div>
-    那么我的选择是 <input v-model="myChoice" readonly/>
+    那么我的选择是 <input v-model="lastChoice" readonly/>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'breakfast',
-  data () {
+  data() {
     return {
-      title: '童茜雅的早餐智能选择器',
+      name: "breakfast",
+      title: "童茜雅的早餐智能选择器",
       date: new Date().toLocaleDateString(),
-      myChoice: '',
-      disabled: null,
+      lastDate: "",
+      lastChoice: "",
+      selected: false,
+      helpText: "帮我选吧！！！",
       options: [
-        {"text": "家里吃"},
-        {"text": "包子"},
-        {"text": "面"},
-        {"text": "馄饨"}
-      ],
-      mounted() {
-          if(localStorage.myChoice) 
-            this.myChoice = localStorage.myChoice;
-          if(localStorage.selected)
-            this.selected = true;
-      },
-      watch:{
-          myChoice(newChoice){
-            localStorage.myChoice = newChoice;
-          },
-          selected(newVal){
-            localStorage.selected = newVal;
-          }
+        { text: "家里吃" },
+        { text: "包子" },
+        { text: "面" },
+        { text: "馄饨" }
+      ]
+    };
+  },
+  mounted() {
+    if (localStorage.lastDate) {
+      if (localStorage.lastDate == new Date().toLocaleDateString()) {
+        this.selected = true;
+        this.helpText = "今天已经选过了！！！";
+      }
+      if (localStorage.lastChoice) {
+        this.lastChoice = localStorage.lastChoice;
       }
     }
   },
+  watch: {
+    lastChoice(newLastChoice) {
+      localStorage.lastChoice = newLastChoice;
+    },
+    lastDate(newLastDate) {
+      localStorage.lastDate = newLastDate;
+    }
+  },
   methods: {
-    helpMe: function(){
-      this.selected = true;
+    helpMe: function() {
       var size = this.options.length;
       var random = Math.floor(Math.random() * size);
-      if(random == 0) {
+      if (random == 0) {
         random = 2;
       }
-      var choice = this.options[random];
-      this.myChoice = choice.text;
+      var choice = this.options[random].text;
+      this.lastChoice = choice;
+      this.lastDate = new Date().toLocaleDateString();
+        this.selected = true;
+        this.helpText = "今天已经选过了！！！";
     }
   }
-}
+};
 </script>
